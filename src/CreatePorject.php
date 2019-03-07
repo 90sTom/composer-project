da43d45acd0bc7274d026d3c9ed4e89f30ffa53d
杨小è¾? <2781568757@qq.com>
2019-03-07 22:35:51 +0800

    0.2.2 init

--- src/CreatePorject.php
+++ src/CreatePorject.php
@@ -16,40 +16,21 @@ use Composer\Repository\InstalledRepositoryInterface;
  */
 class CreatePorject extends LibraryInstaller
 {
-    public function install(InstalledRepositoryInterface $repo, PackageInterface $package)
-    {
-        parent::install($repo, $package);
-        $this->filesystem->removeDirectory($this->getInstallPath($package) . DIRECTORY_SEPARATOR . 'tests');
-    }
 
     /**
      * {@inheritDoc}
      */
-    public function getInstallPath(PackageInterface $package)
+    public function getPackageBasePath(PackageInterface $package)
     {
-
-        if ($this->composer->getPackage()) {
-            $extra = $this->composer->getPackage()->getExtra();
-            if (!empty($extra['yang-xiao-hui'])) {
-                return $extra['yang-xiao-hui'];
-            }
-        }
-
         return 'project';
     }
 
-    public function update(InstalledRepositoryInterface $repo, PackageInterface $initial, PackageInterface $target)
-    {
-        parent::update($repo, $initial, $target);
-      
-        $this->filesystem->removeDirectory($this->getInstallPath($target) . DIRECTORY_SEPARATOR . 'tests');
-    }
 
     /**
      * {@inheritDoc}
      */
     public function supports($packageType)
     {
-        return 'yang-xiao-hui' === $packageType;
+        return 'yang-project' === $packageType;
     }
 }
